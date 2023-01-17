4f4f05c06657ada3cdcce4ca824bf8b6e8fd9410
Your Name <you@example.com>
2023-01-17 01:38:54 -0300

    handleSubmit and handleError

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -7,10 +7,48 @@ import styles from "./Register.module.css";
 import { useState, useEffect } from "react";
 
 function Register() {
+    const [displayName, setDisplayName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmpassoword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError("");
+
+        const user = {
+            displayName,
+            email,
+            password,
+        };
+
+        if (password !== confirmPassword) {
+            setError("As senhas precisam ser iguais!");
+
+            return handleError();
+        }
+        console.log(user);
+
+        setDisplayName("");
+        setEmail("");
+        setPassword("");
+        setConfirmpassoword("");
+    };
+
+    const handleError = () => {
+        console.log(error);
+        setConfirmpassoword("");
+    };
+
     return (
         <div className={styles.container_form}>
             <h1> Cadastre-se para postar</h1>
-            <form className={styles.form_cadastrar}>
+            <form
+                className={styles.form_cadastrar}
+                onSubmit={handleSubmit}
+            >
                 <label>
                     <span>Nome:</span>
                     <input
@@ -18,6 +56,8 @@ function Register() {
                         name="displayName"
                         required
                         placeholder="Nome do usuário"
+                        value={displayName}
+                        onChange={(e) => setDisplayName(e.target.value)}
                     />
                 </label>
                 <label>
@@ -27,8 +67,11 @@ function Register() {
                         name="email"
                         required
                         placeholder="E-mail do usuário"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </label>
+
                 <label>
                     <span>Senha:</span>
                     <input
@@ -36,6 +79,8 @@ function Register() {
                         name="password"
                         required
                         placeholder="Senha do usuário"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </label>
                 <label>
@@ -45,6 +90,8 @@ function Register() {
                         name="confirmpassoword"
                         required
                         placeholder="Confirme sua senha"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmpassoword(e.target.value)}
                     />
                 </label>
                 <button
@@ -55,6 +102,7 @@ function Register() {
                     Cadastrar
                 </button>
             </form>
+            {error && <p className="error">{error}</p>}
         </div>
     );
 }
