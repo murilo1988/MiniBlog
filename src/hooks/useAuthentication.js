3a798f09dda5c0e3a098bdfc303b12a023ba2478
Your Name <you@example.com>
2023-01-17 15:35:29 -0300

    Authentication hook for register user

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,3 +1,5 @@
+import { db } from "../firebase/config";
+
 import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -22,4 +24,51 @@ export const useAuthentication = () => {
     function checkIfIsCancelled() {
         if (cancelled) return;
     }
+
+    const createUser = async (data) => {
+        checkIfIsCancelled();
+
+        setLoading(true);
+        setError("");
+
+        try {
+            const { user } = createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password,
+            );
+
+            await updateProfile(user, {
+                displayName: data.displayName,
+            });
+
+            return user;
+        } catch (error) {
+            console.log(error.message);
+            console.log(typeof error.message);
+
+            let systemErrorMessage;
+            if (error.message.includes("Password")) {
+                systemErrorMessage = "Senha com 6 ou mais caracteris";
+            } else if (error.message.includes("email-already")) {
+                systemErrorMessage = "E-mail já cadastrado";
+            } else {
+                systemErrorMessage =
+                    "Ocorreu um erro, por favor tente mais tarde";
+            }
+            setError(systemErrorMessage);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+
+    return {
+        auth,
+        createUser,
+        error,
+        loading,
+    };
 };
