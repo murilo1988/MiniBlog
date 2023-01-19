f240d3d4ca8f11603f2323ce8fdfc6662fb564d1
Murilo Cabreira <murilocabreira@hotmail.com>
2023-01-19 14:48:31 -0300

    about page link to create post page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { Link } from "react-router-dom";
+
 // CSS
 import styles from "./About.module.css";
 import stylesPattern from "../CSS/pagesIndex.module.css";
@@ -6,10 +9,10 @@ import stylesPattern from "../CSS/pagesIndex.module.css";
 function About() {
     return (
         <div className={stylesPattern.container}>
-            <div className={styles.container}>
-                <h2>
-                    Sobre o mini <span className={styles.brand}>Blog</span>
-                </h2>
+            <h1>
+                Sobre o mini <span className={styles.brand}>Blog</span>
+            </h1>
+            <div className={styles.about}>
                 <p>
                     Este projeto foi criado com o intuito de Criar e controlar
                     as postagens de fotos comentário e likes{" "}
@@ -18,6 +21,9 @@ function About() {
                     Ele utiliza JavaScript com React no front-end e Firebase no
                     back-end
                 </p>
+                <Link to="/post/create">
+                    <button>Criar post</button>
+                </Link>
             </div>
         </div>
     );
