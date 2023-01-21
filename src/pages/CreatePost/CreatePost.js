a11717b8746fd343b1f78bf9c67b42515332c916
Murilo Cabreira <murilocabreira@hotmail.com>
2023-01-21 13:57:14 -0300

    finalizing post page layout

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -19,44 +19,57 @@ export default function CreatePost() {
     };
     return (
         <div className={stylesPattern.container}>
-            <h1>CreatePost</h1>
-            <h2>Criar post</h2>
-            <p>Compartilhe seus momentos </p>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Titulo:</span>
-                    <input
-                        type="text"
-                        name="title"
-                        required
-                        placeholder="Título da postagem"
-                        onChange={(e) => setTitle(e.target.value)}
-                        value={title}
-                    />
-                </label>
-                <label>
-                    <span>URL da imagem:</span>
-                    <input
-                        type="text"
-                        name="image"
-                        required
-                        placeholder="Imagem da postagem"
-                        onChange={(e) => setTitle(e.target.value)}
-                        value={image}
-                    />
-                </label>
-                <label>
-                    <span>Conteúdo:</span>
-                    <textarea
-                        type="text"
-                        name="body"
-                        required
-                        placeholder="Insirao contúdo do post"
-                        onChange={(e) => setBody(e.target.value)}
-                    ></textarea>
-                </label>
-                <label></label>
-            </form>
+            <h1>Criar post</h1>
+            <div className={styles.container_post}>
+                <p>Compartilhe seus momentos </p>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <span>Titulo:</span>
+                        <input
+                            type="text"
+                            name="title"
+                            required
+                            placeholder="Título da postagem"
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}
+                        />
+                    </label>
+                    <label>
+                        <span>URL da imagem:</span>
+                        <input
+                            type="text"
+                            name="image"
+                            required
+                            placeholder="Imagem da postagem"
+                            onChange={(e) => setImage(e.target.value)}
+                            value={image}
+                        />
+                    </label>
+                    <label>
+                        <span>Conteúdo:</span>
+                        <textarea
+                            rows="3"
+                            type="text"
+                            name="body"
+                            required
+                            placeholder="Insirao contúdo do post"
+                            onChange={(e) => setBody(e.target.value)}
+                        ></textarea>
+                    </label>
+                    <label>
+                        <span>Maque suas tags</span>
+                        <input
+                            type="text"
+                            name="tags"
+                            required
+                            placeholder="Crie suas tags"
+                            onChange={(e) => setTags(e.target.value)}
+                            value={tags}
+                        />
+                    </label>
+                    <button>Postar</button>
+                </form>
+            </div>
         </div>
     );
 }
