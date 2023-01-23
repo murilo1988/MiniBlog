d3f9d80a449ddc7d328b50e374ef0df881fcf9ed
Murilo Cabreira <murilocabreira@hotmail.com>
2023-01-23 12:36:26 -0300

    creating layout to search posts by tag

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,29 @@
 import React from "react";
 // CSS
 import stylesPattern from "../CSS/pagesIndex.module.css";
+import styles from "./Home.module.css";
+import { AiOutlineSearch } from "react-icons/ai";
+
+//hookes
+import { useNavigate, link } from "react-router-dom";
+import { useState } from "react";
 
 function Home() {
     return (
         <div className={stylesPattern.container}>
-            <h1>Home</h1>
+            <h1>Postagens mais recentes</h1>
+            <div className={styles.container_home}>
+                <form>
+                    <label>
+                        <input type="text" placeholder="Ou busque por tags" />
+                        <button>
+                            <AiOutlineSearch />
+                        </button>
+                    </label>
+                </form>
+            </div>
         </div>
     );
 }
 
 export default Home;
-
