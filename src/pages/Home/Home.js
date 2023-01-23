57c8d62243d7cbacff51b85ab71eaa93f05ec647
Murilo Cabreira <murilocabreira@hotmail.com>
2023-01-23 12:55:11 -0300

    creating body for when there are no posts yet

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,22 +5,44 @@ import styles from "./Home.module.css";
 import { AiOutlineSearch } from "react-icons/ai";
 
 //hookes
-import { useNavigate, link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
 
 function Home() {
+    const [query, setQuery] = useState("");
+    const [posts] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefautl();
+    };
     return (
         <div className={stylesPattern.container}>
             <h1>Postagens mais recentes</h1>
             <div className={styles.container_home}>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label>
-                        <input type="text" placeholder="Ou busque por tags" />
+                        <input
+                            type="text"
+                            placeholder="Ou busque por tags"
+                            onChange={(e) => setQuery(e.target.value)}
+                            value={query}
+                        />
                         <button>
                             <AiOutlineSearch />
                         </button>
                     </label>
                 </form>
+                <div className={styles.posts_home}>
+                    <h2>Posts</h2>
+                    {!posts && posts.length === 0 && (
+                        <div className={styles.no_posts}>
+                            <p> Não foram encontrados posts</p>
+                            <Link to="/post/create">
+                                <button>Criar primeiro post</button>
+                            </Link>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
