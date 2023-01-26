d6c6cd61520113963b4604b3919c768e6a642e6e
Murilo Cabreira <murilocabreira@hotmail.com>
2023-01-26 16:08:19 -0300

    finishing Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -7,16 +7,18 @@ import { Link } from "react-router-dom";
 //hooks
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 function Dashboard() {
     const { user } = useAuthValue();
     const uid = user.id;
 
-    const {
-        documents: posts,
-        loading,
-        error,
-    } = useFetchDocuments("posts", null, uid);
+    const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+    const { deleteDocument } = useDeleteDocument("posts");
+
+    if (loading) {
+        return <p>Carregando</p>;
+    }
 
     // posts do usuário
 
@@ -33,13 +35,47 @@ function Dashboard() {
                         </Link>
                     </div>
                 ) : (
-                    <div className={styles.posts}>
+                    <div>
                         <p>Gerencie os seus posts</p>
+                        <div className={styles.posts}>
+                            {posts &&
+                                posts.map((post) => (
+                                    <div key={post.id}>
+                                        <div>
+                                            <h3>{post.title}</h3>
+                                        </div>
+                                        <div className={styles.edit_btns}>
+                                            <button>
+                                                <Link
+                                                    to={`/posts/${post.id}`}
+                                                    className={styles.look_post}
+                                                >
+                                                    {" "}
+                                                    Ver Post
+                                                </Link>
+                                            </button>
+                                            <button>
+                                                <Link
+                                                    to={`/posts/edit/${post.id}`}
+                                                    className={styles.edit_post}
+                                                >
+                                                    Editar
+                                                </Link>
+                                            </button>
+                                            <button
+                                                onClick={() =>
+                                                    deleteDocument(post.id)
+                                                }
+                                                className={styles.delete_post}
+                                            >
+                                                Excluir
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                        </div>
                     </div>
                 )}
-
-                {posts &&
-                    posts.map((post) => <h3 key={post.id}>{post.title}</h3>)}
             </div>
         </div>
     );
